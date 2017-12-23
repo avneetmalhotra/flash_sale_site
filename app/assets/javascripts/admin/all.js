b2f3e92293163ece71f0b8e2057fd9c07cbb5973
Avneet Singh Malhotra <avneet@vinsol.com>
2017-12-23 11:14:09 +0530

    admin singnup and login

---
+++ app/assets/javascripts/admin/all.js
@@ -0,0 +1,5 @@
+//= require jquery
+//= require rails-ujs
+//= require bootstrap.min.js
+//= require jquery-ui
+//= require miscellaneous
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
 
+  before_action :render_admin_layout_for_admin
+
   def index
   end
+
+  private
+
+    def render_admin_layout_for_admin
+      render layout: 'admin'
+    end
 end
